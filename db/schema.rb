fb2be44c3b5a235a85b952455117533850f927a0
Yuichi Hira <yuichihira@Yuichi-no-MacBook-Air.local>
2016-04-17 22:08:46 +0900

    usersテーブルにnameカラムをstring型で追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160417094012) do
+ActiveRecord::Schema.define(version: 20160417130640) do
 
   create_table "contents", force: :cascade do |t|
     t.integer  "category_id", limit: 4
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160417094012) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "name",                   limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
