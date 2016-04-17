de45c2fef86361c3791b4abac72159f36dd6cccd
Yuichi Hira <yuichihira@Yuichi-no-MacBook-Air.local>
2016-04-18 00:17:33 +0900

    マイグレーションファイルの実行

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160417130640) do
+ActiveRecord::Schema.define(version: 20160417145709) do
 
   create_table "contents", force: :cascade do |t|
     t.integer  "category_id", limit: 4
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160417130640) do
     t.text     "body",        limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id",     limit: 4
   end
 
   create_table "users", force: :cascade do |t|
