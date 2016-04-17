6dbcfb5273df648bc6b1bfb82318986c790a9a55
Yuichi Hira <yuichihira@Yuichi-no-MacBook-Air.local>
2016-04-18 01:01:52 +0900

    マイページに表示したい情報をshowアクションに定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+  def show
+    @name = current_user.name
+    @contents = Content.where(user_id: current_user.id).page(params[:page]).per(6).order("created_at DESC")
+  end
 end
+
