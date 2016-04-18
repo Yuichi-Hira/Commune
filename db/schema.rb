5ffe2f0c686b57d9be56db99b10ff4ed91d02624
Yuichi Hira <yuichihira@Yuichi-no-MacBook-Air.local>
2016-04-18 15:11:40 +0900

    contentsテーブルにimageカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160417145709) do
+ActiveRecord::Schema.define(version: 20160418060819) do
 
   create_table "contents", force: :cascade do |t|
-    t.integer  "category_id", limit: 4
-    t.string   "title",       limit: 255
-    t.string   "contributor", limit: 255
-    t.text     "image_url",   limit: 65535
-    t.text     "body",        limit: 65535
+    t.integer  "category_id",        limit: 4
+    t.string   "title",              limit: 255
+    t.string   "contributor",        limit: 255
+    t.text     "image_url",          limit: 65535
+    t.text     "body",               limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id",     limit: 4
+    t.integer  "user_id",            limit: 4
+    t.string   "image_file_name",    limit: 255
+    t.string   "image_content_type", limit: 255
+    t.integer  "image_file_size",    limit: 4
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
