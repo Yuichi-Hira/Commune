32b2145ed9852632e66684a943c1b9820aca1727
Yuichi Hira <yuichihira@Yuichi-no-MacBook-Air.local>
2016-04-19 14:58:49 +0900

    contentsのアソシエーションを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @name = current_user.name
-    @contents = Content.where(user_id: current_user.id).page(params[:page]).per(6).order("created_at DESC")
+    @contents = current_user.contents.page(params[:page]).per(6).order('created_at DESC')
   end
 end
 
