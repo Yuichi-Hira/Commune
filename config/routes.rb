cb0d6c67a70df0c377cc794eed5b4b8555093c3e
Yuichi Hira <yuichihira@Yuichi-no-MacBook-Air.local>
2016-04-20 22:53:05 +0900

    コンフリクト解消

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   root 'contents#index'
   resources :contents, only: [:index, :new, :create]
   get   'users/:id'  =>  'users#show'
+<<<<<<< Updated upstream
   delete  'contents/:id'   =>  'contents#destroy'
   get   'contents/:id'  => 'contents#show'
+=======
+>>>>>>> Stashed changes
   get   'contents/:id'  => 'contents#show'
 end
