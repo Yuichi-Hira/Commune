5a40140538d1ab723f5f91f46541325c87b9a3d6
Yuichi Hira <yuichihira@Yuichi-no-MacBook-Air.local>
2016-04-22 12:56:34 +0900

    showアクションのルーティングが重複していたので削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'contents#index'
-  resources :contents, only: [:index, :new, :create]
+  resources :contents, only: [:index, :new, :create, :show]
   get   'users/:id'  =>  'users#show'
 delete  'contents/:id'   =>  'contents#destroy'
-  get   'contents/:id'  => 'contents#show'
   get   'contents/:id'  => 'contents#show'
 end
