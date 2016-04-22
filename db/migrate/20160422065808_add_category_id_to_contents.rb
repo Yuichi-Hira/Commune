460d6c2bf9ef90ebca40cd74ad3664ec9289b80f
Yuichi Hira <yuichihira@Yuichi-no-MacBook-Air.local>
2016-04-22 16:57:02 +0900

    category_idはすでにあるので、重複しないように記述

--- db/migrate/20160422065808_add_category_id_to_contents.rb
+++ db/migrate/20160422065808_add_category_id_to_contents.rb
@@ -1,11 +1,4 @@
 class AddCategoryIdToContents < ActiveRecord::Migration
-def self.up
-    add_column(:contents, :category_id, :integer)
-    add_index(:contents, :category_id)
-  end
-
-  def self.down
-    remove_index(:contents, :column => :category_id)
-    remove_column(:contents, :category_id)
+  def change
   end
 end
