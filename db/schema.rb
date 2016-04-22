c6c4f7fe657c57b9025e323b72160fe8fef5c718
Yuichi Hira <yuichihira@Yuichi-no-MacBook-Air.local>
2016-04-22 15:03:13 +0900

    カテゴリーのnameをstring型で追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160418060819) do
+ActiveRecord::Schema.define(version: 20160422060039) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "contents", force: :cascade do |t|
     t.integer  "category_id",        limit: 4
