ce81145a47a19fa43006af670b9cfe4e33b613cb
Yuichi Hira <yuichihira@Yuichi-no-MacBook-Air.local>
2016-04-22 16:12:36 +0900

    category_idカラムを追加

--- db/migrate/20160422065808_add_category_id_to_contents.rb
+++ db/migrate/20160422065808_add_category_id_to_contents.rb
@@ -1,4 +1,11 @@
 class AddCategoryIdToContents < ActiveRecord::Migration
-  def change
+def self.up
+    add_column(:contents, :category_id, :integer)
+    add_index(:contents, :category_id)
+  end
+
+  def self.down
+    remove_index(:contents, :column => :category_id)
+    remove_column(:contents, :category_id)
   end
 end
