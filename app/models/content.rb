b2c7547372056e5b95bbf51372fc124202ff0523
Yuichi Hira <yuichihira@Yuichi-no-MacBook-Air.local>
2016-04-22 16:29:18 +0900

    has_many,belongs_toの関係を記述

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,6 +1,7 @@
 class Content < ActiveRecord::Base
   validates_presence_of :title
   validates_presence_of :body
+  belongs_to :category
   has_attached_file :image,
                      styles: { medium: "400x600", thmb: "200x300"}
          validates_attachment_content_type :image,
