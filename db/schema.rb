ebadf335c92af4137ac90bc53f8b1feb71b215ff
Yuichi Hira <yuichihira@Yuichi-no-MacBook-Air.local>
2016-04-22 19:11:15 +0900

    categoryテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,11 @@
 ActiveRecord::Schema.define(version: 20160422065808) do
 
   create_table "categories", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.integer "category_id",    limit: 4
+    t.string  "studio",         limit: 255
+    t.string  "creater",        limit: 255
+    t.string  "event_workshop", limit: 255
+    t.string  "item",           limit: 255
   end
 
   create_table "contents", force: :cascade do |t|
