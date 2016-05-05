1a110e253f2281bead695c50762d9b3ae0471433
Yuichi Hira <yuichihira@Yuichi-no-MacBook-Air.local>
2016-05-05 18:25:52 +0900

    name変数が取得できなくなったので削除

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -24,10 +24,8 @@ end
     params.require(:content).permit(:title, :image, :body, :category_id).merge(user_id: current_user.id)
   end
 
-  def move_to_index
+def move_to_index
     redirect_to action: :index unless user_signed_in?
   end
-
-
 
 end
