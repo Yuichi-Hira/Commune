90d4f73729be0ef9e23de24978c65c26fea98510
Yuichi Hira <yuichihira@Yuichi-no-MacBook-Air.local>
2016-05-25 17:01:04 +0900

    Bootstrapを適用

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160422065808) do
+ActiveRecord::Schema.define(version: 20160422060039) do
 
   create_table "categories", force: :cascade do |t|
-    t.integer "category_id",    limit: 4
-    t.string  "studio",         limit: 255
-    t.string  "creater",        limit: 255
-    t.string  "event_workshop", limit: 255
-    t.string  "item",           limit: 255
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "contents", force: :cascade do |t|
